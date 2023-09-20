7372a50d84b1f6e0f06a12000fd20f1b0ae6212b
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-09-20 10:14:05 -0300

    Added ball bouncing and world scene

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -4,7 +4,12 @@ extends RigidBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var is_started : bool = false
 
+func _input(event):
+	if not is_started and event.is_action_pressed("ui_accept"):
+		is_started = true
+		linear_velocity = Vector2(40, -200)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
