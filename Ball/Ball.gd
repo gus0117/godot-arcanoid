d48d5b4d2d8b37dc9b3b914954f29c5049944e49
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-09-22 13:06:10 -0300

    Added logic to colision with blocks

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -5,17 +5,24 @@ extends RigidBody2D
 # var a = 2
 # var b = "text"
 var is_started : bool = false
+var speed_x = 100
+var speed_y = -200
+var plus_speed = 75
+var block_counter = 0 #Contador de bloques destruidos
 
 func _input(event):
 	if not is_started and event.is_action_pressed("ui_accept"):
 		is_started = true
-		linear_velocity = Vector2(40, -200)
+		linear_velocity = Vector2(speed_x, speed_y)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var collision_list = get_colliding_bodies()
+	for body in collision_list:
+		if body.is_in_group("gr_block"):
+			body.queue_free()
+			block_counter += 1
+			if block_counter == 4:
+				speed_x += plus_speed
+				speed_y += plus_speed
+				linear_velocity = Vector2(speed_x, speed_y)
+				block_counter = 0
