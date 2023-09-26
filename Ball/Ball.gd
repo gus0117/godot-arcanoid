291493629cd7a63719e369eb935057f247cfd938
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2023-09-25 22:47:45 -0300

    Fixed infinite loop when the ball take lose option

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -1,17 +1,17 @@
 extends RigidBody2D
 
 var is_started : bool = false
-var speed_x = 100
+var speed_x = 30
 var speed_y = -200
 var plus_speed = 75
 var block_counter = 0 #Contador de bloques destruidos
 
 #Audios
-onready var break_a : AudioStreamPlayer2D = $break_a
-onready var hit_a : AudioStreamPlayer2D = $hit_a
-onready var lose_a : AudioStreamPlayer2D = $lose_a
-onready var start_a : AudioStreamPlayer2D = $start_a
-onready var win_a : AudioStreamPlayer2D = $win_a
+onready var break_a : AudioStreamPlayer2D = get_node("break_a")
+onready var hit_a : AudioStreamPlayer2D = get_node("hit_a")
+onready var lose_a : AudioStreamPlayer2D = get_node("lose_a")
+onready var start_a : AudioStreamPlayer2D = get_node("start_a")
+onready var win_a : AudioStreamPlayer2D = get_node("win_a")
 
 func _input(event):
 	if not is_started and event.is_action_pressed("ui_accept"):
@@ -26,23 +26,26 @@ func _physics_process(delta):
 			body.queue_free()
 			break_a.play()
 			block_counter += 1
-			if body.get_parent().get_child_count() <= 0:
+			if body.get_parent().get_child_count() == 1:
 				print("you win")
 				get_tree().paused = true
 				win_a.play()
 				yield(win_a, "finished")
 				queue_free()
+				var replay_scene = load("res://Title/Replay_title.tscn")
+				get_parent().add_child(replay_scene.instance())
 			if block_counter == 4:
 				speed_x += plus_speed
-				speed_y += plus_speed
+				speed_y -= plus_speed
 				linear_velocity = Vector2(speed_x, speed_y)
 				block_counter = 0
 		elif body.name == "bot_border":
-			var lose_title = load("res://Title/Lose_title.tscn")
-			get_parent().get_node("Panel").add_child(lose_title.instance())
+			print("Lose")
 			get_tree().paused = true
 			lose_a.play()
 			yield(lose_a, "finished")
 			queue_free()
+			var replay_scene = load("res://Title/Replay_title.tscn")
+			get_parent().add_child(replay_scene.instance())
 		else:
 			hit_a.play()
